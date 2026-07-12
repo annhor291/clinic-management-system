102c9b1f6b377eca8bf38c96b2a52d056d1cfe95
An_NHD <nguyenhoangduyan29012@gmail.com>
2026-07-12 18:52:50 +0700

    add cors to securityConfig

--- src/main/java/com/example/clinic/security/SecurityConfig.java
+++ src/main/java/com/example/clinic/security/SecurityConfig.java
@@ -43,7 +43,9 @@ public class SecurityConfig {
                     CorsConfiguration config = new CorsConfiguration();
                     config.setAllowedOrigins(List.of(
                             "http://localhost:5500",
-                            "http://127.0.0.1:5500"
+                            "http://127.0.0.1:5500",
+                            "http://localhost:5173",
+                            "http://127.0.0.1:5173"
                     ));
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(List.of("*"));
